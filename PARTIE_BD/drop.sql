73e085273a747c861001c58728ac2de1021831f0
Lucas Delcroix <lucasdelcroix2005@gmail.com>
2026-04-03 14:05:03 +0200

    bd correspondant au modèle demandé par le prof

---
+++ PARTIE_BD/drop.sql
@@ -0,0 +1,25 @@
+DROP TABLE Reparation;
+DROP TABLE Travaille_Dans_La_Boutique;
+DROP TABLE Est_Affectee_A;
+DROP TABLE Soin;
+DROP TABLE Est_Nourri;
+DROP TABLE Est_Le_Parent_De;
+DROP TABLE Est_Compatible_Avec;
+DROP TABLE Est_Parraine;
+DROP TABLE Chiffre_Affaires;
+
+DROP TABLE Animal;
+DROP TABLE Contrat_Travail;
+DROP TABLE Boutique;
+
+DROP TABLE Enclos;
+DROP TABLE Visiteur;
+DROP TABLE Zone;
+DROP TABLE Personnel;
+
+DROP TABLE Prestataire;
+DROP TABLE Espece;
+DROP TABLE Fonction;
+
+COMMIT;
+
